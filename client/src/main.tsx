470a1c5208319a0c8ac130cf92cd5b867cdd3c30
CerealSK <angelo.motus1122@gmail.com>
2025-03-23 10:21:32 +0000

    Restored to '911ebfc5fc349f0d0e0539fcd0a7e9547edf82f1'
    Replit-Restored-To: 911ebfc5fc349f0d0e0539fcd0a7e9547edf82f1

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,10 @@ import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "./lib/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="light">
-      <App />
-      <Toaster />
-    </ThemeProvider>
+    <App />
+    <Toaster />
   </QueryClientProvider>
 );
